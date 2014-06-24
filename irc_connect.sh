44a3f6cf33548084e4316c8624cbd64fc0f7772f
Rasmus Eskola <fruitiex@gmail.com>
2014-06-24 21:22:59 +0300

    connect script

---
+++ irc_connect.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# passwords to irc servers as variables in this file, like so:
+# IRCPASS_FREENODE="awesomesauce"
+source ~/.ircpw
+
+# NOTE: I use an irc bouncer, so the server is always the same
+# modifying the script for multiple servers should be simple enough though
+SERVER=server.org
+PORT=6667
+
+RECONNECT_DELAY=3
+NICK="JsiiTestUser"
+
+II=$HOME/src/ii/ii
+IRCPATH=$HOME/irc
+
+connect() {
+	while true; do
+		IRCPASS="$2" $II -i $IRCPATH/$1 -s $SERVER -p $PORT -k "IRCPASS" -n $NICK
+		sleep $RECONNECT_DELAY
+	done
+}
+
+connect qnet $IRCPASS_QNET &
+connect freenode $IRCPASS_FREENODE &
+connect hut $IRCPASS_HUT &
