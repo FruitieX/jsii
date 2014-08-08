3cf0cb45bc6133bd54094b1b3307ac4b3d3c81f5
Rasmus Eskola <fruitiex@gmail.com>
2014-08-08 17:52:07 +0300

    WIP: transition to node-vim-readline

--- jsii.js
+++ jsii.js
@@ -8,11 +8,10 @@ var hilight_re = new RegExp(".*" + myNick + ".*", 'i');
 var url_re = /(^|\s)((https?:\/\/)[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi;
 var url_ignore_nicks = [ 'BoWa' ];
 var ansi_escape_re = /\x1b[^m]*m/;
-var readline = require('readline');
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
-var rlv = require('readline-vim');
+var vimrl = require('./vim-readline');
 
 var separatorColor = clc.xterm(239);
 var chanColor = clc.xterm(242);
@@ -52,37 +51,8 @@ var openChan = function(filePath) {
     var chan = chanColor(chan_shortened);
     var chan_insert = chanInsertColor(chan_shortened);
 
-    // prompt printing function, assumes cursor is inside the prompt where node
-    // readline expects it to be. cursor position is restored after printing the
-    // prompt unless cursorAfterPrompt is given, then it's put after the input line
-    var printPrompt = function(cursorAfterPrompt) {
-        if(!cursorAfterPrompt)
-            process.stdout.write('\033[s'); // store cursor position
-
-        process.stdout.write('\r'); // move cursor to beginning of line
-
-        // for multiline inputs move cursor up to prompt before printing
-        var inputLength = num_s.length + chan_shortened.length + rli.cursor + 1;
-        if(inputLength + 1 > process.stdout.columns)
-            process.stdout.write('\033[' + Math.floor(inputLength / process.stdout.columns) + 'A');
-
-        if(vim.isnormal)
-            process.stdout.write(num + chan + ' ');
-        else
-            process.stdout.write(num + chan_insert + ' ');
-        process.stdout.write(rli.line);
-
-        if(!cursorAfterPrompt)
-            process.stdout.write('\033[u'); // restore cursor position
-    };
-
-    var clearPrompt = function() {
-        // move cursor to beginning of prompt first
-        var inputLength = num_s.length + chan_shortened.length + rli.line.length;
-        process.stdout.write('\033[' + Math.floor(process.stdout.rows - inputLength / process.stdout.columns + 1) + ';0H');
-        // clear channel name + prompt
-        process.stdout.write(Array(num_s.length + chan_shortened.length + 1 + rli.line.length + 1).join(' '));
-    };
+    var prompt_s = chan_shortened + ' > ';
+    var prompt_s_ins = chan_shortened + ' i ';
 
     // prints line at current terminal cursor position
     var printLine = function(line) {
@@ -197,8 +167,6 @@ var openChan = function(filePath) {
     // redraw screen
     var redraw = function() {
         process.stdout.write('\u001B[2J\u001B[0;0f'); // clear terminal
-        var inputLength = num_s.length + chan_shortened.length + rli.line.length;
-        process.stdout.write('\033[' + Math.floor(process.stdout.rows - inputLength / process.stdout.columns + 1) + ';0H');
 
         var start = 0;
         while (start < file.length) {
@@ -209,12 +177,8 @@ var openChan = function(filePath) {
             printLine(file.substring(start, end));
             start = end + 1;
         }
-        // make room for the prompt
-        // TODO: why should this be rli.cursor
-        var inputLength = num_s.length + chan_shortened.length + 1 + rli.cursor;
-        for(var i = 0; i < Math.floor(inputLength / process.stdout.columns); i++) {
-            process.stdout.write('\n');
-        }
+
+        readline.redraw();
     };
 
     // limit file to size fileBufSize
@@ -223,73 +187,50 @@ var openChan = function(filePath) {
         file = file.substr(file.indexOf("\n") + 1, file.length);
     }
 
+    process.stdin.setRawMode(true);
+
     // handle keyboard events
-    process.stdin.on('data', function(key) {
-        // 'ctrl-c' = quit
-        if(key == '\u0003') process.exit(0);
-    });
-    process.stdin.on('data', function(key) {
-        var keyHex = key.toString('hex');
+    process.stdin.on('readable', function() {
+        var input = process.stdin.read();
+        if(input) {
+            // 'ctrl-c' = quit
+            if(input == '\u0003') process.exit(0);
 
-        // previous channel (alt + h)
-        if(keyHex === '1b68') process.exit(10);
-        // next channel (alt + l)
-        else if(keyHex === '1b6c') process.exit(11);
-        // jump to this channel (alt + 1-9)
-        else if(keyHex.substring(0, 3) === '1b3' &&
-            keyHex.substring(3) !== '0') process.exit(keyHex.substring(3));
+            var keyHex = input.toString('hex');
 
-        // DEBUG: uncomment this line to find the keycodes
-        //console.log(key.toString('hex'));
-    });
+            // previous channel (alt + h)
+            if(keyHex === '1b68') process.exit(10);
+            // next channel (alt + l)
+            else if(keyHex === '1b6c') process.exit(11);
+            // jump to this channel (alt + 1-9)
+            else if(keyHex.substring(0, 3) === '1b3' &&
+                keyHex.substring(3) !== '0') process.exit(keyHex.substring(3));
+            else
+                readline.handleInput(input);
 
-    var rli = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-
-        // nickname completion from nicknames in file buffer
-        // TODO: get nicks from irc server?
-        completer: function(line) {
-            var word = line.substring(line.lastIndexOf(" ") + 1);
-            var hits = completions.filter(function(c) { return c.indexOf(word) == 0 })
-            for(var i = hits.length - 1; i >= 0; i--) {
-                if(hits[i] === '!' || hits[i] === '***') { // skip status/info nicknames
-                    hits.splice(i, 1);
-                } else if (word === line) { // typing at beginning of line? append colon
-                    hits[i] += ': ';
-                } else { // else append comma
-                    hits[i] += ', ';
-                }
-            }
-            return [hits, word]
+            // DEBUG: uncomment this line to find the keycodes
+            //console.log(key.toString('hex'));
         }
     });
 
-    // clear prompt, print line above prompt, print prompt again and return cursor
-    // to correct position within prompt
-    var printLine_restorePrompt = function(line) {
-        process.stdout.write('\033[s'); // store cursor position
-
-        // clear prompt, then move cursor to beginning of prompt
-        clearPrompt();
-        var inputLength = num_s.length + chan_shortened.length + rli.line.length;
-        process.stdout.write('\033[' + Math.floor(process.stdout.rows - inputLength / process.stdout.columns + 1) + ';0H');
-
-        // print line
-        printLine(line);
-
-        // make room for prompt
-        // TODO: WHY should we use rli.cursor not rli.line.length here?
-        // otherwise we get too many newlines when cursor is positioned on an input
-        // line != the last one. IDGI. :(
-        inputLength = num_s.length + chan_shortened.length + rli.cursor;
-        for(var i = 0; i < Math.floor(inputLength / process.stdout.columns); i++) {
-            process.stdout.write('\n');
+    /*
+    // nickname completion from nicknames in file buffer
+    // TODO: get nicks from irc server?
+    completer: function(line) {
+    }
+    var word = line.substring(line.lastIndexOf(" ") + 1);
+    var hits = completions.filter(function(c) { return c.indexOf(word) == 0 })
+    for(var i = hits.length - 1; i >= 0; i--) {
+        if(hits[i] === '!' || hits[i] === '***') { // skip status/info nicknames
+            hits.splice(i, 1);
+        } else if (word === line) { // typing at beginning of line? append colon
+            hits[i] += ': ';
+        } else { // else append comma
+            hits[i] += ', ';
         }
-
-        printPrompt(true); // print prompt again, but let us handle cursor restoring
-        process.stdout.write('\033[u'); // restore cursor position
-    };
+    }
+    return [hits, word]
+    */
 
     // watch file for changes
     Tail = require('tail').Tail;
@@ -302,46 +243,31 @@ var openChan = function(filePath) {
             file = file.substr(file.indexOf("\n") + 1, file.length);
         }
 
-        printLine_restorePrompt(data);
+        redraw();
     });
 
     // handle terminal resize
     process.stdout.on('resize', function() {
-        var cursorPos = rli.cursor;
         process.stdout.write('\u001B[2J\u001B[0;0f'); // clear terminal
         redraw();
-
-        // hack: print our prompt after node readline prints its prompt ;)
-        setTimeout(function() {
-            printPrompt(true);
-            var inputLength = num_s.length + chan_shortened.length + 1 + rli.line.length;
-            // move cursor to beginning of first prompt line
-            process.stdout.write('\033[' + Math.floor(process.stdout.rows - inputLength / process.stdout.columns + 1) + ';0H');
-
-            // move cursor back to where it was:
-            // down
-            if((num_s.length + chan_shortened.length + 1 + cursorPos) >= process.stdout.columns)
-                process.stdout.write('\033[' + Math.floor((num_s.length + chan_shortened.length + 1 + cursorPos) / process.stdout.columns) + 'B');
-            // right
-            if((num_s.length + chan_shortened.length + 1 + cursorPos) % process.stdout.columns)
-                process.stdout.write('\033[' + (num_s.length + chan_shortened.length + 1 + cursorPos) % process.stdout.columns + 'C');
-        });
     });
 
     // parse some select commands from input line
-    rli.setPrompt(num_s + chan_shortened + ' ');
-    rli.on('line', function(cmd) {
+    readline = vimrl({
+        normalPrompt: prompt_s,
+        normalPromptLen: prompt_s.length,
+        insertPrompt: prompt_s_ins,
+        insertPromptLen: prompt_s_ins.length
+    }, function(line) {
         redraw();
-        printPrompt(true);
 
-        var msg_s = cmd;
-        if(msg_s === '/bl' || msg_s.substring(0, 4) === '/bl ') { // request backlog
-            msg_s = "/privmsg *backlog " + chan_s + ' ' + msg_s.substring(4);
-        } else if(msg_s.substring(0, 4) === '/me ') { // irc ACTION message
-            msg_s = "\001ACTION " + msg_s.substring(4);
-        } else if(msg_s === '/names') { // request nick list
-            msg_s = "/names " + chan_s;
-        } else if (msg_s === '/ul') { // list urls in buffer
+        if(line === '/bl' || line.substring(0, 4) === '/bl ') { // request backlog
+            line = "/privmsg *backlog " + chan_s + ' ' + line.substring(4);
+        } else if(line.substring(0, 4) === '/me ') { // irc ACTION message
+            line = "\001ACTION " + line.substring(4);
+        } else if(line === '/names') { // request nick list
+            line = "/names " + chan_s;
+        } else if (line === '/ul') { // list urls in buffer
             var current = 0;
             var splitFile = file.split('\n');
 
@@ -368,8 +294,8 @@ var openChan = function(filePath) {
             }
 
             return;
-        } else if (msg_s === '/u' || msg_s.substring(0, 3) === '/u ') { // open url
-            var skip = parseInt(msg_s.substring(3)) | 0;
+        } else if (line === '/u' || line.substring(0, 3) === '/u ') { // open url
+            var skip = parseInt(line.substring(3)) | 0;
             var splitFile = file.split('\n');
 
             var url;
@@ -406,7 +332,7 @@ var openChan = function(filePath) {
                     stdio: [ 'ignore', 'ignore' , 'ignore' ]
                 });
                 child.unref();
-                printLine_restorePrompt("xxxx-xx-xx xx:xx <***> URL " + (parseInt(msg_s.substring(3)) | 0) +  " opened: " + url);
+                printLine_restorePrompt("xxxx-xx-xx xx:xx <***> URL " + (parseInt(line.substring(3)) | 0) +  " opened: " + url);
             } else {
                 printLine_restorePrompt("xxxx-xx-xx xx:xx <***> No URL found");
             }
@@ -415,30 +341,13 @@ var openChan = function(filePath) {
         }
 
         // send input line to ii
-        var msg = new Buffer(msg_s + '\n', 'utf8');
+        var msg = new Buffer(line + '\n', 'utf8');
         fs.writeSync(inFile, msg, 0, msg.length, null);
     });
-
-    // vim like keybindings
-    vim = rlv(rli, function() {
-        // callback function prints our prompt whenever readline handles input
-        printPrompt();
-    });
-
-    // custom keymap
-    var map = vim.map;
-    // typing 'jj' quickly toggles to normal mode
-    map.insert('jj', 'esc');
-    // 500 ms delay allowed between j keypresses
-    vim.threshold = 500;
 
     // clear terminal and print file contents at launch
     process.stdout.write('\u001B[2J\u001B[0;0f');
     redraw();
-    printPrompt(true);
-
-    // start in insert mode
-    vim.forceInsert();
 };
 
 openChan(process.argv[2]);
